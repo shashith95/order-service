609c356364c305011f7edfc5c4d3a2e5530d209f
Shashith <Shashith.Aravinda@cloudsolutions.com.sa>
2024-01-02 19:50:25 +0530

    added base service

--- src/test/java/com/learning/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/learning/orderservice/OrderServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.learning.orderservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrderServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
