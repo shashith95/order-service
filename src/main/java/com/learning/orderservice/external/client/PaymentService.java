3ec231c22a540c08692b234da448b13f64fafba7
“Shashith” <saravinda58@gmail.com>
2024-01-14 13:32:37 +0530

    introduced rate limiter

--- src/main/java/com/learning/orderservice/external/client/PaymentService.java
+++ src/main/java/com/learning/orderservice/external/client/PaymentService.java
@@ -1,16 +1,23 @@
 package com.learning.orderservice.external.client;
 
+import com.learning.commondataservice.exception.GeneralException;
 import com.learning.commondataservice.model.common.ApiResponse;
 import com.learning.orderservice.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external-payment", fallbackMethod = "fallback")
 @FeignClient(name = "payment-service/api/v1/payment")
 public interface PaymentService {
 
     @PostMapping
     ResponseEntity<ApiResponse> doPayment(@Valid @RequestBody PaymentRequest paymentRequest);
+
+    default void fallback(Exception e) throws GeneralException {
+        throw new GeneralException("Payment Service is not available", "UNAVAILABLE");
+    }
 }
--- src/main/java/com/learning/orderservice/external/client/ProductService.java
+++ src/main/java/com/learning/orderservice/external/client/ProductService.java
@@ -1,6 +1,8 @@
 package com.learning.orderservice.external.client;
 
+import com.learning.commondataservice.exception.GeneralException;
 import com.learning.commondataservice.model.common.ApiResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,10 +10,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external-payment", fallbackMethod = "fallback")
 @FeignClient(name = "product-service/api/v1/product")
 public interface ProductService {
 
     @PutMapping("update-quantity")
     ResponseEntity<ApiResponse> updateProductQuantity(@Valid @RequestParam @NotNull Long productId,
                                                       @RequestParam @NotNull Long quantity);
+
+    default void fallback(Exception e) throws GeneralException {
+        throw new GeneralException("Payment Service is not available", "UNAVAILABLE");
+    }
 }
